14b06d0bb4bdb468b31ee3f600d2fe60bb3eb1bd
Konstantin S. <havendv@gmail.com>
2026-05-08 06:51:58 +0400

    feat: Updated OpenAPI spec (#5)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ThreeDAIStudio/Generated/ThreeDAIStudio.Models.TripoTextTo3DRequest.g.cs
+++ src/libs/ThreeDAIStudio/Generated/ThreeDAIStudio.Models.TripoTextTo3DRequest.g.cs
@@ -29,6 +29,19 @@ namespace ThreeDAIStudio
         /// <summary>
         /// 
         /// </summary>
+        public bool TryPickSharedGeneration(
+#if NET6_0_OR_GREATER
+            [global::System.Diagnostics.CodeAnalysis.NotNullWhen(true)]
+#endif
+            out global::ThreeDAIStudio.TripoSharedGenerationRequest? value)
+        {
+            value = SharedGeneration;
+            return IsSharedGeneration;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
 #if NET6_0_OR_GREATER
         public global::ThreeDAIStudio.TripoTextTo3DRequestVariant2? TripoTextTo3DRequestVariant2 { get; init; }
 #else
@@ -42,6 +55,19 @@ namespace ThreeDAIStudio
         [global::System.Diagnostics.CodeAnalysis.MemberNotNullWhen(true, nameof(TripoTextTo3DRequestVariant2))]
 #endif
         public bool IsTripoTextTo3DRequestVariant2 => TripoTextTo3DRequestVariant2 != null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool TryPickTripoTextTo3DRequestVariant2(
+#if NET6_0_OR_GREATER
+            [global::System.Diagnostics.CodeAnalysis.NotNullWhen(true)]
+#endif
+            out global::ThreeDAIStudio.TripoTextTo3DRequestVariant2? value)
+        {
+            value = TripoTextTo3DRequestVariant2;
+            return IsTripoTextTo3DRequestVariant2;
+        }
         /// <summary>
         /// 
         /// </summary>
@@ -118,8 +144,8 @@ namespace ThreeDAIStudio
         /// 
         /// </summary>
         public TResult? Match<TResult>(
-            global::System.Func<global::ThreeDAIStudio.TripoSharedGenerationRequest?, TResult>? sharedGeneration = null,
-            global::System.Func<global::ThreeDAIStudio.TripoTextTo3DRequestVariant2?, TResult>? tripoTextTo3DRequestVariant2 = null,
+            global::System.Func<global::ThreeDAIStudio.TripoSharedGenerationRequest, TResult>? sharedGeneration = null,
+            global::System.Func<global::ThreeDAIStudio.TripoTextTo3DRequestVariant2, TResult>? tripoTextTo3DRequestVariant2 = null,
             bool validate = true)
         {
             if (validate)
@@ -143,8 +169,32 @@ namespace ThreeDAIStudio
         /// 
         /// </summary>
         public void Match(
-            global::System.Action<global::ThreeDAIStudio.TripoSharedGenerationRequest?>? sharedGeneration = null,
-            global::System.Action<global::ThreeDAIStudio.TripoTextTo3DRequestVariant2?>? tripoTextTo3DRequestVariant2 = null,
+            global::System.Action<global::ThreeDAIStudio.TripoSharedGenerationRequest>? sharedGeneration = null,
+
+            global::System.Action<global::ThreeDAIStudio.TripoTextTo3DRequestVariant2>? tripoTextTo3DRequestVariant2 = null,
+            bool validate = true)
+        {
+            if (validate)
+            {
+                Validate();
+            }
+
+            if (IsSharedGeneration)
+            {
+                sharedGeneration?.Invoke(SharedGeneration!);
+            }
+            else if (IsTripoTextTo3DRequestVariant2)
+            {
+                tripoTextTo3DRequestVariant2?.Invoke(TripoTextTo3DRequestVariant2!);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Switch(
+            global::System.Action<global::ThreeDAIStudio.TripoSharedGenerationRequest>? sharedGeneration = null,
+            global::System.Action<global::ThreeDAIStudio.TripoTextTo3DRequestVariant2>? tripoTextTo3DRequestVariant2 = null,
             bool validate = true)
         {
             if (validate)
